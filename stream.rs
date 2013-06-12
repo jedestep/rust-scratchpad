45ebfddcfbc1bc084bbe6675acb721cffe107291
Jed Estep <aje@jhu.edu>
2013-06-12 14:36:33 -0400

    additional fail conditions

--- stream.rs
+++ stream.rs
@@ -6,22 +6,32 @@ use std::iterator::IteratorUtil;
 trait Stream<T:Eq> {
 	//Return if the stream has more values.	
 	fn has_next(&self) -> bool;
-	//Move the stream forward by count units.	
+	fn first<'a>(&'a self) -> &'a T;	
+	//Move the stream forward by count units.		
 	fn pass(&mut self, count: int);
 	//Apply a function to the first count units and return the results in a vector.
 	fn process<V: Copy>(&mut self, count: int, f: &fn(&T) -> V) -> ~[V];
+	//Collect the first count elements and return them in a vector.
+	//This is logically equivalent to self.process(count, id), modulo pointer types.
+	fn aggregate(&mut self, count: int) -> ~[T];
 	//Look for the elements of search in the first element of the stream.
 	//If the first element of the stream matches any element, return the first match.	
 	fn expect(&self, search: ~[T]) -> Option<T>; 
 }
 
-impl<T:Eq> Stream<T> for ~[T] {
+impl<T:Eq + Copy> Stream<T> for ~[T] {
 	fn has_next(&self) -> bool {
 		self.len() > 1	
 	}
+	fn first<'a>(&'a self) -> &'a T {
+		if self.is_empty() {
+			fail!("cannot get the first element of an empty stream!");
+		}
+		&'a self[0]
+	}
 	fn pass(&mut self, count: int) {
 		let mut c = 0;
-		if !self.has_next() {
+		if !self.has_next() || count >= self.len() as int {
 			fail!("cannot pass past end of stream!");
 		}	
 		while self.has_next() && c < count {
@@ -29,10 +39,11 @@ impl<T:Eq> Stream<T> for ~[T] {
 			c += 1;
 		}
 	}
+
 	fn process<V: Copy>(&mut self, count: int, f: &fn(&T) -> V) -> ~[V] {
 		let mut c = 0;
 		let mut ret: ~[V] = ~[];
-		if !self.has_next() {
+		if !self.has_next() || count >= self.len() as int {
 			fail!("cannot process past end of stream!");
 		}
 		while self.has_next() && c < count {
@@ -41,6 +52,9 @@ impl<T:Eq> Stream<T> for ~[T] {
 			c += 1;
 		}
 		ret
+	}
+	fn aggregate(&mut self, count: int) -> ~[T] {	
+		self.process(count, |x| *x)
 	}	
 	fn expect(&self, search: ~[T]) -> Option<T> {
 		for search.iter().advance |&choice| {
@@ -61,7 +75,19 @@ mod tests {
 		assert_eq!((~[0]).has_next(), false);
 		assert_eq!((~[0,1,2]).has_next(), true);
 	}
+
+	#[test]
+	fn test_first() {
+		let full = ~[0];
+		assert_eq!(full.first(), &0);
+	}
 	
+	#[test]
+	#[should_fail]
+	fn test_first_fail() {
+		let empty: ~[~str] = ~[];
+		empty.first();
+	}	
 	#[test]
 	fn test_pass() {
 		let mut stream = ~[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
@@ -76,12 +102,30 @@ mod tests {
 	}
 
 	#[test]
+	#[should_fail]
+	fn test_pass_fail() {
+		let mut stream = ~[0,1];
+		stream.pass(1);
+		assert_eq!(stream[0], 1);
+		assert_eq!(stream.has_next(), false);
+		stream.pass(1);
+	}
+
+	#[test]
+	#[should_fail]
+	fn test_pass_runover() {
+		let mut stream= ~[0,1];
+		stream.pass(3);
+	}
+
+	#[test]
 	fn test_process() {
 		let mut stream = ~[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 		let f: &fn(&int) -> int = |&val| 2 * val;
 		assert_eq!(stream.process(3, f), ~[0,2,4]);
 		assert_eq!(stream.process(3, f), ~[6,8,10]);
 	}
+
 	#[test]
 	#[should_fail]
 	fn test_process_fail() {
@@ -90,14 +134,19 @@ mod tests {
 		assert_eq!(stream.process(1, f), ~[0]);
 		stream.process(1, f);
 	}
+
 	#[test]
 	#[should_fail]
-	fn test_pass_failure() {
+	fn test_process_runover() {
 		let mut stream = ~[0,1];
-		stream.pass(1);
-		assert_eq!(stream[0], 1);
-		assert_eq!(stream.has_next(), false);
-		stream.pass(1);
+		let f: &fn(&int) -> int = |&val| 2 * val;
+		stream.process(3, f);
+	}
+	#[test]
+	fn test_aggregate() {
+		let mut stream = ~[0,1,2,3,4,5,6,7,8,9];
+		assert_eq!(stream.aggregate(3), ~[0,1,2]);
+		assert_eq!(stream.aggregate(3), ~[3,4,5]);
 	}
 
 	#[test]
@@ -107,4 +156,5 @@ mod tests {
 		assert_eq!(stream.expect(~[1,0]), Some(0));
 		assert_eq!(stream.expect(~[3,4]), None);
 	}
+
 }
